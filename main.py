f13025715dac6f1e7540e3a69996d18a366be977
tamojit2000 <tamojitdas2000@gmail.com>
2021-12-28 11:27:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import pandas as pd
 from multiprocessing import Process
 import os
 
-def run(filename):
+FINISH,TOTAL=0,0
 
-    f=open('raw/'+filename,'r',encoding='utf-8')
-    xhtml=f.read()
-    f.close()
+def update_progress(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
-    p = HTMLTableParser()
-    p.feed(xhtml)
 
-    data=p.tables
-    # print(len(data)) 19
+def part1(data,filename):
+    global TOTAL,FINISH
 
+
+    TOTAL+=1;update_progress(FINISH,TOTAL,prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     ## data[1] starts ##
-
     df1=pd.DataFrame()
     #print(data[1])
     #print(len(data[1]))
@@ -132,6 +136,13 @@ def run(filename):
 
     df1.to_csv('output/'+filename+'_'+'out1.csv',index=False)
 
+    FINISH+=1;update_progress(FINISH,TOTAL,prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+
+def part2(data,filename):
+    global TOTAL,FINISH
+
+    TOTAL+=1;update_progress(FINISH,TOTAL,prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     ## data[8] starts again ##
 
@@ -143,6 +154,13 @@ def run(filename):
 
     ## data[8] ends again ##
 
+    FINISH+=1;update_progress(FINISH,TOTAL,prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+def part3(data,filename):
+    global TOTAL,FINISH
+
+    TOTAL+=1;update_progress(FINISH,TOTAL,prefix = 'Progress:', suffix = 'Complete', length = 50)
+
     ## data[9] starts ##
 
     cols=data[9][2]
@@ -153,12 +171,36 @@ def run(filename):
 
     ## data[9] ends ##
 
-    print(filename+' completed')
+    FINISH+=1;update_progress(FINISH,TOTAL,prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+def run(filename):
+
+    f=open('raw/'+filename,'r',encoding='utf-8')
+    xhtml=f.read()
+    f.close()
+
+    p = HTMLTableParser()
+    p.feed(xhtml)
+
+    data=p.tables
+    # print(len(data)) 19
+
+    Process(target=part1,args=(data,filename,)).start()
+    Process(target=part2,args=(data,filename,)).start()
+    Process(target=part3,args=(data,filename,)).start()
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
+
     files=os.listdir('raw')
+    print('PROCESSING')
     for i in files:
-        print(i,'Processing')
         Process(target=run,args=(i,)).start()
     print()
 
